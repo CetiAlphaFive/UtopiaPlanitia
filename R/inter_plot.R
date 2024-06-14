a14097c69eb5f73ae0d9c1bad425acffa3be47f2
Jack T. Rametta <jtrametta@ucdavis.edu>
2024-06-13 23:52:18 -0700

    Add interaction plot function

---
+++ R/inter_plot.R
@@ -0,0 +1,48 @@
+#' Interaction Plot for Causal Forest
+#'
+#' Generates an interaction plot for a causal forest model.
+#'
+#' @param c.forest A fitted causal forest model object from the \code{grf} package.
+#' @param x_var The name of the variable to be plotted on the x-axis.
+#' @param y_var The name of the variable to be plotted on the y-axis.
+#' @param bin_count Number of bins for the hexagonal binning. Default is 50.
+#' @param limits The limits of the color scale, by default the minimum and maximum of the CATEs.
+#' @return A ggplot2 object with the interaction plot.
+#' @import ggplot2
+#' @import ggExtra
+#' @export
+#' @examples
+#' \dontrun{
+#' cf_model <- causal_forest(X, Y, W)
+#' inter_plot(cf_model, x_var = "X1", y_var = "X2")
+#' }
+inter_plot <- function(c.forest, x_var, y_var, bin_count = 50, limits = c(min(c.forest$predictions), max(c.forest$predictions))) {
+  plot.df <- data.frame(cates = c.forest$predictions, c.forest$X.orig)
+
+  p <- plot.df |>
+    ggplot(aes_string(x = x_var, y = y_var, z = "cates")) +
+    stat_summary_hex(bins = bin_count) +
+    geom_point(alpha = 0) +
+    scale_fill_gradient2(low = "#fd647c", mid = "#e6e6e6", high = "#3d900e", limits = limits) +
+    theme_minimal() +
+    labs(fill = "CATE") +
+    theme(
+      text = element_text(size = 12, family = "serif"),
+      panel.background = element_rect(fill = '#e6e6e6'),
+      plot.title = element_text(hjust = 0.5),
+      legend.position = "bottom",
+      panel.border = element_blank(),
+      axis.text = element_text(),
+      axis.title = element_text(size = rel(1.2)),
+      strip.text = element_text(hjust = 0),
+      strip.background = element_rect(fill = NA, color = NA),
+      legend.key = element_blank(),
+      complete = TRUE
+    )
+
+  ggExtra::ggMarginal(p, type = "histogram", fill = "#e6e6e6", color = "white")
+}
+
+# Example usage
+# Assume `cf_model` is a fitted causal forest model
+# inter_plot(cf_model, x_var = "X1", y_var = "X2")
