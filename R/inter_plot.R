c3bd10556dbbdb4b58cc9a99f4192adde4ed53a7
Jack T. Rametta <jtrametta@ucdavis.edu>
2024-06-14 00:02:11 -0700

    fix

--- R/inter_plot.R
+++ R/inter_plot.R
@@ -8,8 +8,6 @@
 #' @param bin_count Number of bins for the hexagonal binning. Default is 50.
 #' @param limits The limits of the color scale, by default the minimum and maximum of the CATEs.
 #' @return A ggplot2 object with the interaction plot.
-#' @import ggplot2
-#' @import ggExtra
 #' @export
 #' @examples
 #' \dontrun{
@@ -17,14 +15,25 @@
 #' inter_plot(cf_model, x_var = "X1", y_var = "X2")
 #' }
 inter_plot <- function(c.forest, x_var, y_var, bin_count = 50, limits = c(min(c.forest$predictions), max(c.forest$predictions))) {
+
+  # Check if ggplot2 and ggridges are installed
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    stop("Package 'ggplot2' is required but not installed. Please install it to use this function.")
+  }
+  if (!requireNamespace("ggExtra", quietly = TRUE)) {
+    stop("Package 'ggExtra' is required but not installed. Please install it to use this function.")
+  }
+
+  requireNamespace("ggplot2")
+  requireNamespace("ggExtra")
+
   plot.df <- data.frame(cates = c.forest$predictions, c.forest$X.orig)
 
   p <- plot.df |>
-    ggplot(aes_string(x = x_var, y = y_var, z = "cates")) +
+    ggplot2::ggplot(aes_string(x = x_var, y = y_var, z = "cates")) +
     stat_summary_hex(bins = bin_count) +
     geom_point(alpha = 0) +
     scale_fill_gradient2(low = "#fd647c", mid = "#e6e6e6", high = "#3d900e", limits = limits) +
-    theme_minimal() +
     labs(fill = "CATE") +
     theme(
       text = element_text(size = 12, family = "serif"),
